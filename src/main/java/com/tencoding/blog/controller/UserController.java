6e0cd1c4d5499056dd439d78652103722662a9f4
bde574786 <102010541+bde574786@users.noreply.github.com>
2022-06-17 15:21:02 +0900

    로그인화면 , 회원가입화면

---
+++ src/main/java/com/tencoding/blog/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.tencoding.blog.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    // .../blog/user/loing-Form
+    @GetMapping("/login_form")
+    public String loingForm() {
+
+        return "user/login_form";
+    }
+
+    @GetMapping("/join_form")
+    public String joinForm() {
+
+        return "user/join_form";
+    }
+}
