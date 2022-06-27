5ad4f2e2f71c4fda61049a82dc64a0b4e49a2414
bde574786 <102010541+bde574786@users.noreply.github.com>
2022-06-27 11:56:46 +0900

    글 쓰기 완료

--- src/main/java/com/tencoding/blog/controller/BoardController.java
+++ src/main/java/com/tencoding/blog/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.tencoding.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class BoardController {
 	
@@ -11,5 +14,12 @@ public class BoardController {
 		
 		return "home";
 	}
+	
+	@GetMapping("/board/save_form")
+	public String saveForm() {
+		log.info("saveForm 메서드 호출");
+		return "/board/save_form";		
+	}
+	
 	
 }
