87b1f43a5354d5e3a31fa6cc12567a4c34f41bc9
bde574786 <102010541+bde574786@users.noreply.github.com>
2022-06-30 12:39:11 +0900

    페이징 처리

--- src/main/java/com/tencoding/blog/controller/BoardController.java
+++ src/main/java/com/tencoding/blog/controller/BoardController.java
@@ -27,35 +27,48 @@ public class BoardController {
 	BoardService boardService;
 	
 	@GetMapping({"", "/"})
-	public String index(Model model, @PageableDefault(size = 5, 
-		sort = "id", direction = Direction.DESC) Pageable pageable) {
-		
-		Page<Board> pageBoards = boardService.getBoardList(pageable);
-		
-		// [1 2 3 4 5 '6' 7 8 9 10]
-		// 1. 현재 페이지에 앞 뒤로 5 블록(칸)씩 보여야 한다.
-		// 2. 현재 페이지에 'active' 하기
-		// 3. 페이지 버튼을 누르면(블록) 해당 페이지로 화면을 이동해야 한다.
-		
-		int startPage = pageBoards.getPageable().getPageNumber() - 5;
-		int endPage = pageBoards.getPageable().getPageNumber() + 5;
-		
-		
-		// 시작 페이지를 설정해야한다.
-		
-		// 페이지 번호를 배열로 만들어서 던져주기
+	public String index(Model model, @PageableDefault(size = 5 , 
+				sort = "id", direction = Direction.DESC) Pageable pageable) {
+		Page<Board> pageBoards =  boardService.getBoardList(pageable);
+
+		// [ 1 2 3 4 5 '6' 7 8 9 10]
+
+		// 1. 현재 페이지 앞 뒤로 2 블록(칸)씩 보여야 한다.
+		// 2. 페이지 버튼을 누르면(블록) 해당 페이지로 화면을 이동해야 한다.
+		// 3. 현재 페이지에 'active' 하기 
+
+
+//		int startPage = pageBoards.getPageable().getPageNumber() - 2;
+//		int endPage = pageBoards.getPageable().getPageNumber() + 2;
+
+		int nowPage = pageBoards.getPageable().getPageNumber() + 1;
+		int startPage = Math.max(nowPage - 2, 1); // 두 int 값 중에 큰 값을 반환 합니다. 
+		int endPage = Math.min(nowPage + 2, pageBoards.getTotalPages());
+		System.out.println("------------------------");
+		log.info("현재 화면의 블록 숫자(현재 페이지) : {} ", nowPage);
+		log.info("현재 화면의 보여질 블록의 시작 번호 : {} ", startPage);
+		log.info("현재 화면의 보여질 마지막 블록의 번호 : {} ", endPage);
+		log.info("화면에 보여줄 총 게시글 / 한 화면에 보여질 게시 글 (총 페이지 숫자)  : {} ", pageBoards.getTotalPages());
+		System.out.println("------------------------");
+
+
+
+		// 페이지 번호를 배열로 만들어서 던져 주기 !!! 
 		ArrayList<Integer> pageNumbers = new ArrayList<>();
-		// 마지막 번호 까지 저장하기
-		for (int i = 0; i <= endPage; i++) {
+		// 주의 !!! (마지막 번호까지 저장하기)
+		for (int i = startPage; i <= endPage; i++) {
 			pageNumbers.add(i);
 		}
-		
+
+
+		// 시작 페이지를 설정해야 한다. 
+
 		model.addAttribute("pageable", pageBoards);
 		model.addAttribute("startPage", startPage);
 		model.addAttribute("endPage", endPage);
-//		model.addAllAttributes("pageNumbers" pageNumbers);
-		
-		
+		model.addAttribute("pageNumbers", pageNumbers);
+
+		model.addAttribute("pageable", boardService.getBoardList(pageable));
 		return "index";
 	}
 	
