a56f9bf7f1631255e251f9b4cd629601c8add4d4
bde574786 <102010541+bde574786@users.noreply.github.com>
2022-07-04 16:53:24 +0900

    글 수정 csrf 적용

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -84,6 +84,9 @@ let index = {
 	
 	update: function() {
 		
+		let token = $("meta[name='_csrf']").attr("content");
+		let header = $("meta[name='_csrf_header']").attr("content");
+		
 		let boardId = $("#id").val();
 		
 		let data = {
@@ -92,6 +95,10 @@ let index = {
 		}	
 		
 		$.ajax({
+			beforeSend : function(xhr) {
+				console.log("xhr : " + xhr);
+				xhr.setRequestHeader(header, token)
+			},
 			type: "PUT",
 			url: "/api/board/" + boardId,
 			data: JSON.stringify(data),
